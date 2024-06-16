c995554a5ee9afefd140b4b1c6cfce73a3eaf461
qwertyjack12 <zelenin.2016@bk.ru>
2024-06-16 23:11:35 +1000

    add console start

--- src/main/java/com/application/diploma/DiplomaApplication.java
+++ src/main/java/com/application/diploma/DiplomaApplication.java
@@ -1,12 +1,21 @@
 package com.application.diploma;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@PropertySource("classpath:application.properties")
 public class DiplomaApplication {
+
+	@Value("${db.username}")
+	private String dbUsername;
+
+	@Value("${db.password}")
+	private String dbPassword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiplomaApplication.class, args);
